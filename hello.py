39adfb7c78065a405aae26b4b2065d89fda8140e
Brendan <bmleds1275@gmail.com>
2017-05-30 08:35:53 -0400

    I've been messing around with flask

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('hello.html')
+
+
+@app.route("/staff")
+def yup():
+    return "Something about dogs"
+
+@app.route("/bootstrap")
+def boot():
+    return render_template('bootstrap.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=80, debug=True)
